e71c366bc9334f9c9be55c57bacc6a16fa8a9e4a
Paul Campbell <nimbleltd@gmail.com>
2016-04-19 00:09:24 -0500

    connected BookDetail to Redux via mapSateToProps this closes #12

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
 
+
 export default class App extends Component {
   render() {
     return (
